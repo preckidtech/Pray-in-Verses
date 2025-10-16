36b2a9ee49aa2c7f45882f9ae9cce7ee6eefd52e
SirTimi <tobatimilehin1@gmail.com>
2025-10-16 18:13:10 +0100

    updates to the code

--- api/src/main.ts
+++ api/src/main.ts
@@ -12,8 +12,7 @@ async function bootstrap() {
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+  await app.listen(process.env.PORT || 3000);
 
-  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
